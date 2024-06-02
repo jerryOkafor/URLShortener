16b3da1d29b2667680f006c3e0e4487fd8672f57
Jerry Okafor <jerryhanksokafor@gmail.com>
2024-06-02 11:36:00 -0600

    fix : docker

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@ gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
   --output .env prod.env.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
-  --output web/.env.local wbe/prod.env.gpg
+  --output web/.env.local web/prod.env.gpg
